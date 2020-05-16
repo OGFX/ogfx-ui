4f5152ebde21a3eeaea6fde1dc71870a3c8de00c
Florian Paul Schmidt <mista.tapas@gmx.net>
2020-05-16 10:34:09 +0200

    aha, something works!

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@
 from distutils.core import setup
 
 setup(
-    name='ogfx-ui',
-    version='0.1',
-    scripts=['ogfx-ui.py',],
+    name = 'ogfx-ui',
+    version = '0.1',
+    scripts = ['ogfx-ui.py',],
+    packages = ['ogfx'],
+    data_files = [
+        ('views', [ 'views/add_unit.tpl', 'views/index.tpl' ]),
+        ('static', [ 'static/index.css', 'static/range.css' ])
+    ]
 )
 
