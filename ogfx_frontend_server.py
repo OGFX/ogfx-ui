90a8630ba6f13db1de1a0e983c84f1963aa97f54
Florian Paul Schmidt <mista.tapas@gmx.net>
2022-09-16 14:32:35 +0200

    add caching support

--- ogfx_frontend_server.py
+++ ogfx_frontend_server.py
@@ -9,6 +9,7 @@ import json
 arguments_parser = argparse.ArgumentParser(description='ogfx_ui - a web interface for OGFX')
 arguments_parser.add_argument('--log-level', type=int, dest='log_level', help='5: DEBUG, 4: INFO, 3: WARNING, 2: ERROR, 1: CRITICAL, default: %(default)s', action='store', default=4)
 arguments_parser.add_argument('--setup', dest='setup', action='store', help='A file containing a setup to load at startup')
+arguments_parser.add_argument('--lv2-cache', dest='lv2_cache', action='store', help='A file containing the output of ogfx_lv2ls. If missing a scan will be performed at startup')
 arguments = arguments_parser.parse_args()
 
 log_levels_map = {5: logging.DEBUG, 4: logging.INFO, 3: logging.WARNING, 2: logging.ERROR, 1: logging.CRITICAL}
@@ -28,11 +29,15 @@ logging.info('using setups path {}'.format(ogfx_ui.setups_path))
 logging.info('using racks path {}'.format(ogfx_ui.racks_path))
 logging.info('using units path {}'.format(ogfx_ui.units_path))
 
-logging.info('scanning for lv2 plugins...')
-lv2_world_json_string = subprocess.check_output(['ogfx_lv2ls'])
-logging.debug('parsing json')
-lv2_world = json.loads(lv2_world_json_string)
-logging.info('number of plugins: {}'.format(len(lv2_world)))
+if arguments.lv2_cache:
+  with open(arguments.lv2_cache, 'r') as f:
+    lv2_world = json.load(f)
+else:
+  logging.info('scanning for lv2 plugins...')
+  lv2_world_json_string = subprocess.check_output(['ogfx_lv2ls'])
+  logging.debug('parsing json')
+  lv2_world = json.loads(lv2_world_json_string)
+  logging.info('number of plugins: {}'.format(len(lv2_world)))
 
 # og = ogfx_ui.backends.jalv(lv2_world)
 logging.info('starting backend...')
